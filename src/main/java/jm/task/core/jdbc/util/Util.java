76567b79d9c10e04ee33344a38d0e4232cf3f296
Айрат Латыпов <ayrat.latypov.2020@bk.ru>
2024-04-11 17:06:53 +0300

    fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -8,12 +8,14 @@ public class Util {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/test";
     private static final String DB_USERNAME = "root";
     private static final String DB_PASSWORD = "9211218719Mm";
+    private static Connection connection;
     public static Connection getConnection() {
-        Connection connection;
-        try {
-            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         }
         return connection;
     }
